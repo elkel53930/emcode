9b7814f6f72c4c8765cf3bec681cfdb64d062c44
Kazuki Iida <elkel53930@gmail.com>
2024-03-16 12:43:03 +0900

    initial commit
    Signed-off-by: Kazuki Iida <elkel53930@gmail.com>

---
+++ bin/src/main.rs
@@ -0,0 +1,40 @@
+use emcode::*;
+
+fn main()
+{
+
+    let test_script = "let $x = 123.456;
+    let $s = 0.0;
+    let $last = 0.0;
+    
+    if ($x > 1.0){
+        $s = $x
+    } else {
+        $s = 1.0
+    };
+    
+    let $flag = 1;
+    
+    while $flag {
+        $last = $s;
+        $s = ((($x / $s) + $s) / 2.0);
+        if ($s >= $last) {
+            $flag = 0
+        } else {
+            pass
+        }
+    }";
+            let mut env = interpreter::Environment::new();
+            let parser = parser::p_statement();
+            let mut s = parser::ParserState::new(test_script);
+            let result = parser(&mut s);
+            match result {
+                Ok(statement) => {
+                    let _ = interpreter::evaluate_statement(&mut env, &statement);
+                }
+                Err(e) => {
+                    println!("Error at {} : {}", s.pos_string(), e);
+                }
+            }
+        println!("{:?}",env.variables);
+}
---
+++ src/interpreter.rs
@@ -0,0 +1,203 @@
+use std::collections::HashMap;
+
+type VariableName = String;
+type Int = i32;
+type Float = f32;
+type Str = String;
+
+#[derive(Debug)]
+pub enum BinaryOperator {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+    Lt,
+    Le,
+    Gt,
+    Ge,
+    Eq,
+    Neq,
+    And,
+    Or,
+}
+
+#[derive(Debug)]
+pub enum Expression {
+    IntLiteral(Int),
+    FloatLiteral(Float),
+    StringLiteral(Str),
+    Variable(VariableName),
+    FunctionCall(String, Vec<Expression>),
+    BinaryOperation(BinaryOperator, Box<Expression>, Box<Expression>),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Value {
+    Int(Int),
+    Float(Float),
+    Str(Str),
+}
+
+pub struct Environment{
+    pub variables: HashMap<VariableName, Value>,
+    pub functions: HashMap<String, Box<dyn BuildInFunction>>,
+}
+
+impl Environment{
+    pub fn new() -> Environment{
+        Environment{
+            variables: HashMap::new(),
+            functions: HashMap::new(),
+        }
+    }
+}
+
+pub trait BuildInFunction{
+    fn execute(&self, args: &Vec<Value>) -> Result<Value, String>;
+    fn name(&self) -> &str;
+}
+
+fn call_built_in_function(env: &Environment, name: &str, args: &Vec<Value>) -> Result<Value, String> {
+    let function = env.functions.get(name).ok_or(format!("Function not found: {}", name))?;
+    function.execute(args)
+}
+
+pub fn evaluate_expression(
+    environment: &Environment,
+    expression: &Expression,
+) -> Result<Value, String> {
+    match expression {
+        Expression::IntLiteral(i) => Ok(Value::Int(*i)),
+        Expression::FloatLiteral(f) => Ok(Value::Float(*f)),
+        Expression::StringLiteral(s) => Ok(Value::Str((*s.clone()).to_string())),
+        Expression::Variable(v) => {
+            // if the variable is not found, return an error
+            environment
+                .variables
+                .get(v)
+                .ok_or(format!("Variable not found: {}", v))?;
+            Ok(environment.variables[v].clone())
+        }
+        Expression::FunctionCall(name, args) => {
+            let mut evaluated_args = Vec::new();
+            for arg in args {
+                evaluated_args.push(evaluate_expression(environment, arg)?);
+            }
+            let mut values = Vec::new();
+            for arg in evaluated_args {
+                match arg {
+                    Value::Int(i) => values.push(Value::Int(i)),
+                    Value::Float(f) => values.push(Value::Float(f)),
+                    Value::Str(s) => values.push(Value::Str(s)),
+                }
+            }
+            call_built_in_function(environment, name, &values)
+        }
+        Expression::BinaryOperation(op, lhs, rhs) => {
+            let lhs = evaluate_expression(environment, lhs)?;
+            let rhs = evaluate_expression(environment, rhs)?;
+            match (lhs, rhs) {
+                (Value::Int(lhs), Value::Int(rhs)) => match op {
+                    BinaryOperator::Add => Ok(Value::Int(lhs + rhs)),
+                    BinaryOperator::Sub => Ok(Value::Int(lhs - rhs)),
+                    BinaryOperator::Mul => Ok(Value::Int(lhs * rhs)),
+                    BinaryOperator::Div => Ok(Value::Int(lhs / rhs)),
+                    BinaryOperator::Mod => Ok(Value::Int(lhs % rhs)),
+                    BinaryOperator::Lt => Ok(Value::Int((lhs < rhs) as i32)),
+                    BinaryOperator::Le => Ok(Value::Int((lhs <= rhs) as i32)),
+                    BinaryOperator::Gt => Ok(Value::Int((lhs > rhs) as i32)),
+                    BinaryOperator::Ge => Ok(Value::Int((lhs >= rhs) as i32)),
+                    BinaryOperator::Eq => Ok(Value::Int((lhs == rhs) as i32)),
+                    BinaryOperator::Neq => Ok(Value::Int((lhs != rhs) as i32)),
+                    BinaryOperator::And => Ok(Value::Int((lhs != 0 && rhs != 0) as i32)),
+                    BinaryOperator::Or => Ok(Value::Int((lhs != 0 || rhs != 0) as i32)),
+                },
+                (Value::Float(lhs), Value::Float(rhs)) => match op {
+                    BinaryOperator::Add => Ok(Value::Float(lhs + rhs)),
+                    BinaryOperator::Sub => Ok(Value::Float(lhs - rhs)),
+                    BinaryOperator::Mul => Ok(Value::Float(lhs * rhs)),
+                    BinaryOperator::Div => Ok(Value::Float(lhs / rhs)),
+                    BinaryOperator::Lt => Ok(Value::Int((lhs < rhs) as i32)),
+                    BinaryOperator::Le => Ok(Value::Int((lhs <= rhs) as i32)),
+                    BinaryOperator::Gt => Ok(Value::Int((lhs > rhs) as i32)),
+                    BinaryOperator::Ge => Ok(Value::Int((lhs >= rhs) as i32)),
+                    BinaryOperator::Eq => Ok(Value::Int((lhs == rhs) as i32)),
+                    BinaryOperator::Neq => Ok(Value::Int((lhs != rhs) as i32)),
+                    BinaryOperator::And => Ok(Value::Int((lhs != 0.0 && rhs != 0.0) as i32)),
+                    BinaryOperator::Or => Ok(Value::Int((lhs != 0.0 || rhs != 0.0) as i32)),
+                    _ => Err(format!("Invalid operation for float: {:?}", op)),
+                },
+                (Value::Str(lhs), Value::Str(rhs)) => match op {
+                    BinaryOperator::Add => {
+                        let mut result = lhs.clone();
+                        result.push_str(&rhs);
+                        Ok(Value::Str(result))
+                    }
+                    _ => Err(format!("Invalid operation for string: {:?}", op)),
+                },
+                _ => Err(format!("Invalid operation: {:?}", op)),
+            }
+        }
+    }
+}
+
+// statement
+
+#[derive(Debug)]
+pub enum Statement {
+    Expression(Expression),
+    VariableDeclaration(VariableName, Expression),
+    Sequence(Vec<Statement>),
+    Assign(VariableName, Expression),
+    If(Expression, Box<Statement>, Box<Statement>),
+    While(Expression, Box<Statement>),
+    Pass,
+}
+
+pub fn evaluate_statement(
+    environment: &mut Environment,
+    statement: &Statement,
+) -> Result<(), String> {
+    match statement {
+        Statement::Expression(expression) => {
+            evaluate_expression(environment, expression)?;
+            Ok(())
+        }
+        Statement::VariableDeclaration(name, expression) => {
+            let value = evaluate_expression(environment, expression)?;
+            environment.variables.insert(name.clone(), value);
+            Ok(())
+        }
+        Statement::Sequence(statements) => {
+            for statement in statements {
+                evaluate_statement(environment, statement)?;
+            }
+            Ok(())
+        }
+        Statement::Assign(name, expression) => {
+            environment
+                .variables
+                .get(name)
+                .ok_or(format!("Variable not found: {}", name))?;
+            let value = evaluate_expression(environment, expression)?;
+            environment.variables.insert(name.clone(), value);
+            Ok(())
+        }
+        Statement::If(condition, then_branch, else_branch) => {
+            let condition = evaluate_expression(environment, condition)?;
+            match condition {
+                Value::Int(0) => evaluate_statement(environment, else_branch),
+                _ => evaluate_statement(environment, then_branch),
+            }
+        }
+        Statement::While(condition, body) => {
+            let mut result = Ok(());
+            while evaluate_expression(environment, condition)?.clone() != Value::Int(0) {
+                result = evaluate_statement(environment, body);
+            }
+            result
+        }
+        Statement::Pass => Ok(()),
+    }
+}
